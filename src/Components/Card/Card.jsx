c6ba0a799361fddb1adccd62d138e8840e7aef7a
Kieran <kieran.webdevelopment@gmail.com>
2023-08-23 17:35:02 +0100

    card functionality complete

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,24 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMugSaucer } from '@fortawesome/free-solid-svg-icons';
+
+function Card({
+  icon = <FontAwesomeIcon icon={faMugSaucer} />,
+  title = 'insert title',
+  text = 'insert text',
+  iconBgColor = '#3F75FE',
+  ...rest
+}) {
+  return (
+    <div className="card" {...rest}>
+      <div className="icon-container" style={{ background: `${iconBgColor}` }}>
+        {icon}
+      </div>
+      <div className="card-text">
+        <h3>{title}</h3>
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- src/Components/CardContainer/CardContainer.jsx
+++ src/Components/CardContainer/CardContainer.jsx
@@ -1,13 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { faCode } from '@fortawesome/free-solid-svg-icons';
+import Card from '../Card/Card';
 
 // style={{color: '#fff'}} /
 function CardContainer() {
   return (
     <div>
       <h1>yooo</h1>
-      <FontAwesomeIcon icon={faCode} />
+
+      <Card
+        icon={<FontAwesomeIcon icon={faCode} />}
+        title="Easy Deployment"
+        text="Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis."
+      />
     </div>
   );
 }
