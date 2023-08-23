a61a74cb40e066d2461913624cf13bded7d05fff
Kieran <kieran.webdevelopment@gmail.com>
2023-08-23 12:48:22 +0100

    finished task 2

--- src/Components/Banners/Banners.jsx
+++ src/Components/Banners/Banners.jsx
@@ -8,25 +8,31 @@ function Banners() {
       <h2>Banner</h2>
       <Banner
         status="success"
-        title="testin yo"
-        text="lorem ipsum to the maxxxxxxxxxxxxxx"
+        title="Congratulations"
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."
       ></Banner>
       <Banner
         status="warning"
-        title="testin yo"
-        text="lorem ipsum to the maxxxxxxxxxxxxxx"
+        title="Attention"
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum."
       ></Banner>
       <Banner
         status="error"
-        title="testin yo"
-        text="lorem ipsum to the maxxxxxxxxxxxxxx"
+        title="There is a problem with your application"
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum."
       ></Banner>
       <Banner
         status="neutral"
-        title="testin yo"
-        text="lorem ipsum to the maxxxxxxxxxxxxxx"
+        title="Update available"
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."
       ></Banner>
-      <Banner status="neutral" title="testin yo"></Banner>
+      <Banner status="success" title="Congratulations"></Banner>
+      <Banner status="warning" title="Attention"></Banner>
+      <Banner
+        status="error"
+        title="There is a problem with your application"
+      ></Banner>
+      <Banner status="neutral" title="Update available"></Banner>
     </div>
   );
 }
